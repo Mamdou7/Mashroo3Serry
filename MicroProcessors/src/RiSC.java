bb68258b346520d13eb6889eb631fcfc79ae47ba
Mamdou7 <Mamdou111@gmail.com>
2014-11-24 17:46:55 +0200

    bedaya!

---
+++ MicroProcessors/src/RiSC.java
@@ -0,0 +1,19 @@
+
+public class RiSC {
+
+	/**
+	 * @param args
+	 */
+	
+	int[] registers = new int[8];
+	Cache[] caches;
+	int[]memory = new int[64];
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
